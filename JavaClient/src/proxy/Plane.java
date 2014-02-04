d7113e1728a7cc655c89317b9168e1a897442e39
LPTK <lionel.parreaux@gmail.com>
2014-02-04 21:14:02 +0100

    java client draft

---
+++ JavaClient/src/proxy/Plane.java
@@ -0,0 +1,11 @@
+package proxy;
+
+import PlaneModel;
+
+public class Plane {
+	PlaneModel model;
+	
+	int getID() {
+		return model.id;
+	}
+}
---
+++ JavaClient/src/proxy/Proxy.java
@@ -0,0 +1,12 @@
+
+package proxy;
+
+import java.util.List;
+
+public class Proxy {
+	
+	List<Plane> getPlanes() {
+		return null; // TODO
+	}
+
+}
