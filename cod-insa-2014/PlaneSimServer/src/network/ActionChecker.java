e3a359cbfeef45144b96efdec895e3a656de5d16
Nicolas Vailliet <nicolas.vailliet@gmail.com>
2014-02-12 15:22:09 -0500

    Oops forgot a file

---
+++ cod-insa-2014/PlaneSimServer/src/network/ActionChecker.java
@@ -0,0 +1,22 @@
+package network;
+
+import game.Sim;
+import genbridge.Action;
+
+public class ActionChecker {
+
+	Sim ps;
+	
+	public ActionChecker(Sim planeSim)
+	{
+		this.ps = planeSim;
+	}
+	
+	public boolean isValid(Action a)
+	{
+		//TODO is an action valid?
+		return false;
+	}
+	
+	
+}
