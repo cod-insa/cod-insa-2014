d81219463c834fc1e9dcd672d413d56cd2a5ece4
Timothé VIOT <timisare@hotmail.fr>
2014-03-08 22:54:46 +0100

    With a refactoring, this is better...

--- cod-insa-2014/PlaneSimProxy/src/ai/ConsoleAI.java
+++ cod-insa-2014/PlaneSimProxy/src/ai/ConsoleAI.java
@@ -21,8 +21,8 @@ public class ConsoleAI extends AbstractAI
 		Scanner in = new Scanner(System.in);
 		
 		while (true) {
-			serveur.updateSimFrame();
-			ArrayList<BaseModel.View> bases = serveur.getBases();
+			server.updateSimFrame();
+			ArrayList<BaseModel.View> bases = server.getBases();
 			int i;
 
 			System.out.print("Next base: ");
@@ -35,13 +35,13 @@ public class ConsoleAI extends AbstractAI
 			
 			BaseModel.View b = bases.get(i);
 			
-			for (PlaneModel.View p : serveur.getMyPlanes())
+			for (PlaneModel.View p : server.getMyPlanes())
 			{
 				MoveCommand mc = new MoveCommand(p.id(), b.position());
 				
 				System.out.println("Sending command "+mc);
 				
-				serveur.sendCommand(mc);
+				server.sendCommand(mc);
 			}
 			
 		}
