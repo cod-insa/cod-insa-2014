019b3c926548f1e2be88458890c8d7801a98fbf3
LPTK <lionel.parreaux@gmail.com>
2014-03-27 21:04:05 +0100

    Fixed ShallowViewable

--- cod-insa-2014/PlaneSimCommon/src/common/ShallowViewable.java
+++ cod-insa-2014/PlaneSimCommon/src/common/ShallowViewable.java
@@ -1,11 +1,12 @@
 package common;
 
-public interface ShallowViewable<V extends Viewable.View, SV extends Viewable.View> extends Viewable<V> {
+//public interface ShallowViewable<V extends ShallowViewable.View, SV extends Viewable.View> extends Viewable<V> {
+public interface ShallowViewable <SV extends ShallowViewable.ShallowView> {
 	
 	public static interface ShallowView {
 		
 	}
 	
-	public SV getShallowView();
-
+	public SV shallowView();
+	
 }
