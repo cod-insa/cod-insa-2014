5d071b1fecd58f4aa5f3ec71735dbafa52e9f230
Nicolas Vailliet <nicolas.vailliet@gmail.com>
2014-04-01 15:59:34 -0400

    Bases are displayed on web interface Planes are moving (refresh every second) A color by team 6 teams supported

--- cod-insa-2014/PlaneSimInterface/js/client.js
+++ cod-insa-2014/PlaneSimInterface/js/client.js
@@ -1,12 +1,20 @@
+//Nicolas Vailliet
+//Client for CodINSA final contest interface
+//Plane simulation
 
+//Two snapshot from the server to perform smooth animation
+var lastSnap;
+var nextSnap;
+
+//last JSON received from the server
+var json;
+
+//Connect to the server and handle messages coming from it
 var initServerConnection = function () {
-
-    var input = $('#input');
 
     // if user is running mozilla then use its built-in WebSocket
     window.WebSocket = window.WebSocket || window.MozWebSocket;
-
-    var connection = new WebSocket('ws://192.168.1.89:14588');
+    var connection = new WebSocket('ws://localhost:14588');
 
     connection.onopen = function () {
         // connection is opened and ready to use
@@ -24,22 +32,122 @@ var initServerConnection = function () {
 
     connection.onmessage = function (message) {
         // try to decode json (I assume that each message from server is json)
-        try {
-            var json = JSON.parse(message.data);
+	console.log("message received");
+	json = 0;        
+	try {
+            json = JSON.parse(message.data);
         } catch (e) {
             console.log('This doesn\'t look like a valid JSON: ', message.data);
             return;
         }
-        console.log("message received");
-	console.log(message.data);
-        // handle incoming message
+
+	if(json != 0)
+	{
+		
+		if(json.map)
+		{
+
+			//basic map info
+			map_name = json.map.name;
+			base_count = json.map.basecount;
+			center_lat = json.map.latitude;
+			center_long = json.map.longitude;
+			zoom_level = json.map.zoom;
+			initialisationMaps();
+			
+			//adding bases to this map
+			for(var i = 0 ; i<base_count ; i++){
+				
+				var current_base = json.map.bases[i];
+				//console.log("draw base "+current_base.cityname);
+				//console.log(base_icon[current_base.ownerid]);
+				basesArray.push(new google.maps.Marker({
+		    			position: new google.maps.LatLng(current_base.latitude,current_base.longitude),
+		    			map: mymap,
+		    			icon:base_icon[current_base.ownerid],
+		    			title:current_base.cityname
+				}));
+			}
+		}
+
+		if(json.snap && !lastSnap)
+		{
+			lastSnap = json;
+
+			//Look at ownerid to refresh bases colors
+			/* for(var i = 0 ; i<base_count ; i++){
+				basesArray[i].icon = base_icon[json.snap.bases[i].ownerid];
+			}*/
+
+			 //Look at planes
+			 /*for(var j = 0 ; j<json.snap.planes.length ; j++){
+
+				var current_plane = json.snap.planes[j];
+				planesArray.put(current_plane.ownerid*10000+current_plane.id,new google.maps.Marker({
+		    			position: new google.maps.LatLng(current_plane.latitude,current_plane.longitude),
+		    			map: mymap,
+		    			icon:fighter_icon[current_plane.ownerid],
+		    			title:"plane attacking"
+				}));
+			}*/
+
+		}
+
+		if(json.snap && lastSnap)
+		{
+			 nextSnap = json;
+
+			//Look at ownerid to refresh bases colors
+			 for(var i = 0 ; i<base_count ; i++){
+				basesArray[i].icon = base_icon[json.snap.bases[i].ownerid];
+			}
+
+			 //Look at planes
+			 for(var j = 0 ; j<json.snap.planes.length ; j++){
+
+				var current_plane = json.snap.planes[j];
+				var key = current_plane.ownerid*10000+current_plane.id;
+				var getExisting = planesArray.get(key);
+				
+				if(getExisting === undefined)
+				{
+					//console.log("defining");
+					planesArray.put(key,new google.maps.Marker({
+		    			position: new google.maps.LatLng(current_plane.latitude,current_plane.longitude),
+		    			map: mymap,
+		    			icon:fighter_icon[current_plane.ownerid],
+		    			title:"plane attacking"
+					}));
+
+				}
+				else
+				{
+					//console.log("updating");
+					getExisting.position = new google.maps.LatLng(current_plane.latitude,current_plane.longitude);
+					getExisting.title = "plane updated";
+					getExisting.setMap(mymap);
+				}
+
+			}
+
+				//TODO
+				// orientation, bullets...
+
+			 lastSnap = nextSnap;
+		}
+
+		
+
+
+        }
+
+	//console.log(message.data);
     };
     
-     /*** Send mesage when user presses Enter key*/
-	input.keydown(function(e) {
+	/*input.keydown(function(e) {
 		// send the message as an ordinary text
-		connection.send(e.keyCode);
-	});
+		//connection.send(e.keyCode);
+	});*/
     
 };
 
--- cod-insa-2014/PlaneSimInterface/js/map.js
+++ cod-insa-2014/PlaneSimInterface/js/map.js
@@ -2,35 +2,48 @@
 //Map for CodINSA final contest interface
 //Plane simulation
 
-/**
-*	TODO
-*	get map info from server
-*	add planes and bases images
-*	lock map position
-**/
+var mymap;
 
-//Map info
-var map;
-
-//France
-var center_lat = 48.138;
-var center_long = 2.0;
-var zoom_level = 6;
-
-//US
-//var center_lat = 39.0;
-//var center_long = -94.0;
-//var zoom_level = 5;
-
+//Map Info from server
+var map_name;
+var base_count;
+var center_lat;
+var center_long;
+var zoom_level;
 
 //Objects to draw
 var basesArray = new Array();	
-var planesArray = new Array();
+var planesArray = new HashMap();
+
 
 //Images
-var plane1 = '../img/plane1.png';
-var plane2 = '../img/plane2.png';
-var base = '../img/home.png';
+
+var base_icon = new Array(
+	"../img/home/home_white.png", 
+	"../img/home/home_red.png", 
+	"../img/home/home_green.png", 
+	"../img/home/home_blue.png", 
+	"../img/home/home_yellow.png", 
+	"../img/home/home_pink.png", 
+	"../img/home/home_black.png");
+
+var fighter_icon = new Array(
+	"../img/army/air_force_white.png", 
+	"../img/army/air_force_red.png", 
+	"../img/army/air_force_green.png", 
+	"../img/army/air_force_blue.png", 	
+	"../img/army/air_force_yellow.png", 
+	"../img/army/air_force_pink.png", 
+	"../img/army/air_force_black.png");
+
+var civilian_icon = new Array(
+	"../img/people/plane_white.png", 
+	"../img/people/plane_red.png", 
+	"../img/people/plane_green.png", 
+	"../img/people/plane_blue.png", 	
+	"../img/people/plane_yellow.png", 
+	"../img/people/plane_pink.png", 
+	"../img/people/plane_black.png");
 
 //Initialization	
 function initialisationMaps(){
@@ -53,103 +66,8 @@ function initialisationMaps(){
 		}
 
 		 // Map creation with options
-		map = new google.maps.Map(document.getElementById("mapcontain"), myOptions);
-		map.setOptions({draggable: true, zoomControl: false, scrollwheel: false, disableDoubleClickZoom: true});
-		google.maps.event.clearInstanceListeners(map);
-		
-		// To add the marker to the map, use the 'map' property
-		var marker = new google.maps.Marker({
-		    position: new google.maps.LatLng(48.5632,-1.5896),
-		    map: map,
-		    icon:plane1,
-		    title:"Rennes-Plane1"
-		});
+		mymap = new google.maps.Map(document.getElementById("mapcontain"), myOptions);
+		mymap.setOptions({draggable: true, zoomControl: false, scrollwheel: false, disableDoubleClickZoom: true});
+		google.maps.event.clearInstanceListeners(mymap);
+}
 
-		// To add the marker to the map, use the 'map' property
-		var marker = new google.maps.Marker({
-		    position: new google.maps.LatLng(48.2345,1.8963),
-		    map: map,
-		    icon:plane2,
-		    title:"Rennes-Plane2"
-		});
-		
-				// To add the marker to the map, use the 'map' property
-		var marker = new google.maps.Marker({
-		    position: new google.maps.LatLng(48.120955,-1.635003),
-		    map: map,
-		    icon:base,
-		    title:"Rennes-Home"
-		});
-
- }
- 
- 
- 
- 
- 		//Paramètres communs des cercles
-		/*var optionsCercles = {
-			center: maLatlng,
-			clickable: false,
-			editable: false,
-			fillColor: "rgb("+r+","+g+","+b+")",
-			fillOpacity: 0.5,
-			map: carte,
-			radius: 10,
-			strokeWeight:0,
-			visible:false,
-			zIndex:0.9
-		}*/
-					 		
-		//Création de max_post_display cercles
-		/*for(var i = 0 ; i<max_post_display ; i++){
-					cercle = new google.maps.Circle(optionsCercles);
-					circlesArray[i] = cercle;
-		}*/
-		
- //Update du rayon de recherche des posts suivant zoom de la carte
- /*function updateByZoom(){
-	window.mapsLooper.stop();
-	var zoom = carte.getZoom();
-	if(zoom>10) rayon = 10*kilometre*(20-zoom); else rayon = 10000*kilometre;
-	window.mapsLooper.start();
-}*/
- 
- //Ecran d'attente de chargement de la page
- /*function chargement(){
-	var contenantMap = getElementById("maptemplate");
-	var contenantLoad = getElementById("loadtemplate");
-	if( contenantMap && contenantLoad ){
-	contenantLoad.setAttribute("style","display:none");
-	contenantMap.removeAttribute("style");
-	}
- }*/
- 
- 		 // event on zooming
-		 /*google.maps.event.addListener(carte, "zoom_changed", function() {
-			updateByZoom();	});*/
-			
- //Taille, couleur et transparance suivant date et rank.
- /*function optionsByDateAndRank(rank,date,index,circle){
-	
-	//Date by Tranparency 
-	//circle.fillOpacity = 1-(date/time);
-	//Paramètre de bordure Bordure
-	//circle.strokeWeight = 0.5;//px
-	//circle.strokeColor = "black";
-	
-	//Initialement : radius = 10m
-	circle.radius = 10*(rank+1);
-	
-	//Couleur de remplissage
-	//Couleur rouge du site : 206 30 66
-	index = Math.floor(index/10);
-	var red = r;
-	var green = g;
-	var blue = b;
-	if(red+index>255) red=255; else red+=index;
-	if(green+index>255) green=255; else green+=index;
-	if(blue+index>255) blue=255; else blue+=index;
-	circle.fillColor = "rgb("+red+","+green+","+blue+")";
-	//alert("rgb("+red+","+green+","+blue+")");
-	circle.visible = true;
- }*/
