2dce0597946519ebdb54ce2ecbabf3970b6cbc77
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-04-18 10:29:18 +0200

    Add C++ Thrift

--- cod-insa-2014/PlaneSimC++/main.cpp
+++ cod-insa-2014/PlaneSimC++/main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
-#include <list>
-#include <map>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TTransportUtils.h>
 
 #include "models.h"
+#include "./gen-cpp/CommandReceiver.h"
 
-int main(void)
-{
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+using namespace std;
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+using namespace genbridge;
+
+int main(int argc, char** argv) {
+  boost::shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
+  boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+  boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+  CommandReceiverClient  client(protocol);
+
+  try {
+    transport->open();
+
+    /* client.ping(); */
+    printf("ping()\n");
+
+    transport->close();
+  } catch (TException &tx) {
+    printf("ERROR: %s\n", tx.what());
+  }
+
 }
