6236b362cb866c1b3b2df768bbff74d914d09a96
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-04-20 00:27:53 +0200

    Add parametrized duels and grouping

--- cod-insa-2014/Eval/test/EvaluatorSpec.hs
+++ cod-insa-2014/Eval/test/EvaluatorSpec.hs
@@ -10,5 +10,8 @@ main = hspec spec
 spec :: Spec
 spec = do
     describe "duels" $ do
-        it "should be 30 (6*5) different combinaisons" $ do
-            (length . nub) duels `shouldBe` 30
+        it "should be 90 (6*5*1*3) different combinaisons" $ do
+            (length . nub) duels `shouldBe` 90
+    describe "aggregate" $ do
+        it "should be 45 (6*5*1*3/2) different combinaisons" $ do
+            (length . aggregate) duels `shouldBe` 45
