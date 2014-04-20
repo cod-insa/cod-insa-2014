794b236c44d3c01fbd01f95c62b822c6a0d6cf9a
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-04-20 03:47:38 +0200

    Aggregate duelists

--- cod-insa-2014/Eval/test/EvaluatorSpec.hs
+++ cod-insa-2014/Eval/test/EvaluatorSpec.hs
@@ -10,5 +10,5 @@ main = hspec spec
 spec :: Spec
 spec = do
     describe "duels" $ do
-        it "should be 30 (6*5) different combinaisons" $ do
-            (length . nub) duels `shouldBe` 30
+        it "should be 15 (6*5*1*3/3/2) different combinaisons" $ do
+            length duels `shouldBe` 15
