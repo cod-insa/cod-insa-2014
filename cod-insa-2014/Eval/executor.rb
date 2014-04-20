02fd7619906c3f33d0c33a820436daf995a223d1
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-04-20 15:17:08 +0200

    Add some final jars

--- cod-insa-2014/Eval/executor.rb
+++ cod-insa-2014/Eval/executor.rb
@@ -1,35 +1,44 @@
 cities = {}
-cities[1] = { :name => 'Lyon',                  :prog => 'ruby',    :win => 0 }
-cities[2] = { :name => 'Rennes',                :prog => 'ruby',    :win => 0 }
-cities[3] = { :name => 'Strasbourg',            :prog => 'ruby',    :win => 0 }
-cities[4] = { :name => 'Toulouse',              :prog => 'ruby',    :win => 0 }
-cities[5] = { :name => 'Rouen',                 :prog => 'ruby',    :win => 0 }
-cities[6] = { :name => 'Centre Val de Loire',   :prog => 'ruby',    :win => 0 }
+cities[1] = { :name => 'Lyon', :prog => 'cd /home/black/programmation/C++/codinsa2014/cod-insa-2014/AIs_Final/CodINSALyon && ./run.sh localhost ' }
+cities[2] = { :name => 'Rouen', :prog => 'cd /home/black/programmation/C++/codinsa2014/cod-insa-2014/AIs_Final/insaRouen && java -jar rouenIA.jar localhost ' }
+cities[3] = { :name => 'Strasbourg', :prog => 'cd /home/black/programmation/C++/codinsa2014/cod-insa-2014/AIs_Final/strasbourg_vous_fait_perdre && java -jar ai.jar localhost ' }
+cities[4] = { :name => 'Toulouse', :prog => 'cd /home/black/programmation/C++/codinsa2014/cod-insa-2014/AIs_Final/rouen && java -jar rouenIA.jar localhost ' } # TODO
+cities[5] = { :name => 'Rennes', :prog => 'cd /home/black/programmation/C++/codinsa2014/cod-insa-2014/AIs_Final/rouen && java -jar rouenIA.jar localhost ' } # TODO
+cities[6] = { :name => 'Centre Val de Loire',  :prog => 'cd /home/black/programmation/C++/codinsa2014/cod-insa-2014/AIs_Final/CDL && java -jar monIA.jar localhost ' } # Compile
 
-maps = ['france']
+mapfile = 'cuba'
 
-times = [100, 1000, 3600]
+ROOT = '/home/black/programmation/C++/codinsa2014/public-2014'
+TIME = 300
 
-battles = []
-[1,2,3,4,5,6].combination(2).to_a.each do |o|
-    tmp = []
-    times.each do |t|
-        maps.each do |m|
-            tmp << { :op => o, :time => t, :map => m }
-            tmp << { :op => o.reverse, :time => t, :map => m }
-        end
+# battles = []
+# [1,2,3,4,5,6].combination(2).to_a.each do |o|
+#     tmp = []
+#     maps.each do |m|
+#         tmp << { :op => o, :map => m }
+#         tmp << { :op => o.reverse, :map => m }
+#     end
+#     battles << tmp
+# end
+
+# battles.each do |bs|
+    # bs.each do |b|
+    ip = 9090
+    players = ""
+    ARGV.each do |c|
+        players << '"' << cities[c.to_i][:name] << '" ' << ip.to_s << ' '
+        ip = ip + 1
+        ip = 9092 if ip == 9091
     end
-    battles << tmp
-end
+    puts "cd #{ROOT} && java -jar server.jar #{mapfile} #{TIME} #{players} &"
+    puts "sleep 4"
 
-battles.each do |bs|
-    bs.each do |b|
-        ip = 9090
-        players = ""
-        b[:op].each do |op|
-            players << cities[op][:name] << ' ' << ip.to_s << ' '
-            ip = ip + 1
-        end
-        puts "java -jar server.jar #{b[:map]} #{b[:time].to_s} #{players}"
+    ip = 9090
+    ARGV.each do |c|
+        tip = ip.to_s
+        ip = ip + 1
+        ip = 9092 if ip == 9091
+        puts "#{cities[c.to_i][:prog]} #{tip} &"
     end
-end
+    puts "sleep 1000"
+# end
